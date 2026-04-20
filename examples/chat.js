ed0e085bf1441d9bb921b30083a7fa6c8ff69a82
a <102840087+kingdudely@users.noreply.github.com>
2026-04-20 09:13:38 -0400

    Update MessagingService initialization method

--- examples/chat.js
+++ examples/chat.js
@@ -1,6 +1,6 @@
 import MessagingServiceFactory from "messagingservicejs";
 
-const MessagingService = await new MessagingServiceFactory(
+const MessagingService = await MessagingServiceFactory.InitializeAsync(
 	"_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|...", // .ROBLOSECURITY
 	69_420_1337_80085 // Universe ID
 );
