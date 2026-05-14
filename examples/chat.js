e3a00921983af475b260ea149d6b1f5d889d8b5d
a <102840087+kingdudely@users.noreply.github.com>
2026-05-14 09:40:40 -0400

    Refactor chat.js for MessagingService usage
    Refactor MessagingService initialization and chat handling.

--- examples/chat.js
+++ examples/chat.js
@@ -1,14 +1,16 @@
 import MessagingServiceFactory from "messagingservicejs";
 
-const MessagingService = await MessagingServiceFactory.InitializeAsync(
-	"_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|...", // .ROBLOSECURITY
-	69_420_1337_80085 // Universe ID
-);
+const universeId = 69_420_1337_80085;
+const ROBLOSECURITY = "_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|...";
 
-function onChat(message) {
-	console.log("Received:", message);
-}
+const MessagingService = new MessagingServiceFactory(ROBLOSECURITY, universeId);
+await MessagingService.ConnectAsync();
 
-await MessagingService.SubscribeAsync("chat", onChat);
-await MessagingService.PublishAsync("chat", "Hello!");
-await MessagingService.UnsubscribeAsync("chat", onChat);
+let unsubscribeAsync
+unsubscribeAsync = await MessagingService.SubscribeAsync("chat", async (message) => {
+        console.log("Received message:", message.Data);
+        await unsubscribeAsync(); // or await MessagingService.UnsubscribeAsync("chat", callback)
+        await MessagingService.DisconnectAsync();
+});
+
+await MessagingService.PublishAsync("chat", "Yo, wassup bro?!");
